d2b468ff1fa00e53f7611bf009568adf4984be51
Connor Lam <connorlam92@gmail.com>
2026-01-18 15:26:49 -0800

    fixed navbar hamburger by using react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from './pages/Experience';
 import Projects from './pages/Projects';
 import Hobbies from './pages/Hobbies';
 import Footer from './components/Footer';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,30 @@
 import { Link } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
-function Navbar() {
+function AppNavbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-      <div className="container">
+    <Navbar expand="lg" bg="light" className="shadow-sm">
+      <Container>
         <div className="row justify-content-center w-100">
-          <div className="col-12 col-md-10 col-lg-8 d-flex align-items-center ms-2">
-            <Link to="/" className="navbar-brand fs-3 me-4">
+          <div className="col-12 col-md-10 col-lg-8 d-flex align-items-center justify-content-between flex-wrap flex-lg-nowrap">
+            
+            <Navbar.Brand as={Link} to="/" className="fs-3 mb-0">
               Connor Lam
-            </Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link to="/experience" className="nav-link">
-                    Experience
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/projects" className="nav-link">
-                    Projects
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/hobbies" className="nav-link">
-                    Hobbies
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="w-100 w-lg-auto text-end">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/experience">Experience</Nav.Link>
+                <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                <Nav.Link as={Link} to="/hobbies">Hobbies</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+
           </div>
         </div>
-      </div>
-    </nav>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default AppNavbar;
